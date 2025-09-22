50282269f0a7d57dc3096090ca4d5ca83da6e7d1
Groxar <lrdolucho@gmail.com>
2025-09-22 00:01:51 -0400

    Added important gitignored modules

---
+++ android/src/main/java/com/epicness/jalcon/android/AndroidLauncher.java
@@ -0,0 +1,18 @@
+package com.epicness.jalcon.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.epicness.jalcon.JalconGame;
+
+/** Launches the Android application. */
+public class AndroidLauncher extends AndroidApplication {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+        configuration.useImmersiveMode = true; // Recommended, but not required.
+        initialize(new JalconGame(), configuration);
+    }
+}
